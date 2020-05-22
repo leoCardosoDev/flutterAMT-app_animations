b38b62a781385f52dbb0ba962a15714deceaa551
leoCardosoDev <leocardosodev@gmail.com>
2020-05-22 18:23:05 -0300

    First Commit

---
+++ android/app/src/main/java/com/pillbiz/animationsapp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.pillbiz.animationsapp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/exemples/01-implicita_ani.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ImplicitaAni extends StatefulWidget {
+  @override
+  _ImplicitaAniState createState() => _ImplicitaAniState();
+}
+
+class _ImplicitaAniState extends State<ImplicitaAni> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
